f5d079b50537dab0cf62ce5cf42f91124ee2f0e7
ZYL1210 <870138612@qq.com>
2023-05-12 19:14:22 +0800

    blog

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,7 +1,7 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-    "/",
+  { text: "主页", icon: "discovery", link: "/" },
     { text: "分类", icon: "fenlei", link: "/category/" },
     { text: "标签", icon: "tag", link: "/tag/" },
     { text: "时间轴", icon: "timeAxis", link: "/timeline/" },
