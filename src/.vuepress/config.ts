f8ef00bdc7944834978d2dde8963e1ed94e963b2
ZYL1210 <60984434+870138612@users.noreply.github.com>
2023-05-15 02:02:18 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,10 @@ import theme from "./theme.js";
 import {searchProPlugin} from "vuepress-plugin-search-pro";
 export default defineUserConfig({
   base: "/",
-  head: [
+  head: [["meta", { name: "robots", content: "all" }],
+    ["meta",{"http-equiv": "Cache-Control",content: "no-cache, no-store, must-revalidate",} ],
+    ["meta", { "http-equiv": "Pragma", content: "no-cache" }],
+    ["meta", { "http-equiv": "Expires", content: "0" }],  
     // 设置 favor.ico，.vuepress/public 下
     [
       'link', { rel: 'icon', href: '/favicon.ico' }
