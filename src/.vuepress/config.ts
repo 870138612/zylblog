258e47ed3798dc6406c0ac282fa6fd34fd9d36a3
ZYL1210 <870138612@qq.com>
2023-05-21 12:52:21 +0800

    添加文章JVM垃圾收集器

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,11 +4,12 @@ import {searchProPlugin} from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
-  head: [["meta", {name: "robots", content: "all"}],
-    ["meta", {"http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate",}],
-    ["meta", {"http-equiv": "Pragma", content: "no-cache"}],
+  head: [
+      // ["meta", {name: "robots", content: "all"}],
+    // ["meta", {"http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate",}],
+    // ["meta", {"http-equiv": "Pragma", content: "no-cache"}],
 
-    ["meta", {"http-equiv": "Expires", content: "0"}],
+    // ["meta", {"http-equiv": "Expires", content: "0"}],
     // 设置 favor.ico，.vuepress/public 下
     [
       'link', {rel: 'icon', href: '/assets/icon/favicon.ico'}
