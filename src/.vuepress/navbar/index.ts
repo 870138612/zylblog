02167e2fadbc361b77cfbc85abe764df13a4445d
ZYL1210 <870138612@qq.com>
2023-09-01 01:10:47 +0800

    修正计算机网络
    Signed-off-by: ZYL1210 <870138612@qq.com>

--- src/.vuepress/navbar/index.ts
+++ src/.vuepress/navbar/index.ts
@@ -1,7 +1,7 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  { text: "导航", icon: "discovery", link: "/home.md" },
+  { text: "导航", icon: "daohang", link: "/home.md" },
   { text: "文章", icon: "articlelist", link: "/article/" },
   { text: "分类", icon: "fenlei", link: "/category/" },
   { text: "标签", icon: "tag", link: "/tag/" },
