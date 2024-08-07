729af189fc8fe0a0da9294e0222ea38a897a10af
ZYL1210 <870138612@qq.com>
2024-08-07 23:26:04 +0800

    还原config.js设置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -12,6 +12,7 @@ export default defineUserConfig({
   base: "/",
   head: [
     ["meta", {name: "robots", content: "all"}],
+    ["meta", {"http-equiv": "Cache-Control", content: "no-cache, no-store, must-revalidate",}],
     ["meta", {"http-equiv": "Pragma", content: "no-cache"}],
     ["meta", {"http-equiv": "Expires", content: "0"}],
     // 设置 favor.ico，.vuepress/public 下
@@ -23,8 +24,8 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "zh-CN",
-      title: "Zzz",
-      description: "Zzz",
+      title: "LIANG",
+      description: "LiangBlog",
     },
   },
   theme,
