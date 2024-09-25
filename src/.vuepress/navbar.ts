428e94570d7a25acb44331a1868e6d28a4e5023c
ZYL1210 <870138612@qq.com>
2024-09-25 21:32:44 +0800

    更新数学笔记

--- src/.vuepress/navbar.ts
+++
@@ -1,9 +0,0 @@
-import { navbar } from "vuepress-theme-hope";
-
-export const zhNavbar = navbar([
-  { text: "导航", icon: "daohang", link: "/home.md" },
-  { text: "文章", icon: "articlelist", link: "/article/" },
-  { text: "分类", icon: "fenlei", link: "/category/" },
-  { text: "标签", icon: "tag", link: "/tag/" },
-  { text: "时间轴", icon: "timeline", link: "/timeline/" },
-]);
